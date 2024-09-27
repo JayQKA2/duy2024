62a9e394323339db84ac72b1009ff8f6f2db4d89
Minh Duy <kingloki2004@gmail.com>
2024-09-27 23:33:21 +0700

    Practical Work 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -8,6 +9,12 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import vn.edu.usth.weather.Fragment.WeatherAdapter;
 
@@ -43,6 +50,29 @@ public class WeatherActivity extends AppCompatActivity {
         tabLayout.getTabAt(1).setText(getString(R.string.tab_hanoi));
         tabLayout.getTabAt(2).setText(getString(R.string.tab_paris));
         tabLayout.getTabAt(3).setText(getString(R.string.tab_spain));
+
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.rickroll);
+//        mediaPlayer.start();
+        try (InputStream inputStream = getResources().openRawResource(R.raw.rickroll);
+             OutputStream outputStream = new FileOutputStream(new File(getFilesDir(), "rickroll.mp3"))) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error copying media file", e);
+        }
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(new File(getFilesDir(), "rickroll.mp3").getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            Log.e(TAG, "Error playing media file", e);
+        }
+
     }
 
     @Override
